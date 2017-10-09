90dad476076bf72f4ba7148b26f98bb31caf7d84
eedmunds <edmundselliot@gmail.com>
2017-10-09 19:22:32 -0400

    Focus on table

--- scrape.py
+++ scrape.py
@@ -7,6 +7,6 @@ page_content = requests.get(url)
 page_html = page_content.content
 clean_html = BeautifulSoup(page_html)
 
-info_table = clean_html.find(
+info_table = clean_html.find('tbody', attrs={'class':'infobox vcard'})
 
-print clean_html
+print info_table.prettify()
