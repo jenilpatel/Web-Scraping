853b0d86337da3bce57057386e0412ba6f19622d
Jenil Patel <patel489@purdue.edu>
2017-10-09 19:48:05 -0400

    focused table working

--- scrape.py
+++ scrape.py
@@ -7,6 +7,6 @@ page_content = requests.get(url)
 page_html = page_content.content
 clean_html = BeautifulSoup(page_html)
 
-info_table = clean_html.find('tbody', attrs={'class':'infobox vcard'})
+info_table = clean_html.find('table', attrs={'class':'infobox vcard'})
 
 print info_table.prettify()
