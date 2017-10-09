dad7851ea9240d8200d4ef9a8a54a626bf2ca78e
Jenil Patel <patel489@purdue.edu>
2017-10-09 19:10:45 -0400

    Cleaner Html

--- scrape.py
+++ scrape.py
@@ -1,6 +1,10 @@
 import requests
+from BeautifulSoup import BeautifulSoup
 
 url = 'https://en.wikipedia.org/wiki/Rockwell_Collins'
 page_content = requests.get(url)
+
 page_html = page_content.content
-print page_html
+clean_html = BeautifulSoup(page_html)
+
+print clean_html
