449e4babe9915796b5e9395bf1ef5fc5d05ca88c
Jenil Patel <patel489@purdue.edu>
2017-10-09 21:03:27 -0400

    completed without title

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
+import csv
 import requests
 from BeautifulSoup import BeautifulSoup
+from itertools import izip
 
 url = 'https://en.wikipedia.org/wiki/Rockwell_Collins'
 page_content = requests.get(url)
@@ -8,5 +10,27 @@ page_html = page_content.content
 clean_html = BeautifulSoup(page_html)
 
 info_table = clean_html.find('table', attrs={'class':'infobox vcard'})
+title = info_table.findAll('caption')
+print title
+logo = 0
+count = 0
+list_of_headers = []
+list_of_data = []
 
-print info_table.prettify()
+for row in info_table.findAll('tr'):
+	for cell in row.findAll('th'):
+		heading =  cell.text.replace('&ndsp;','')
+		heading = cell.text.replace('&#160;',' ')
+		heading = heading.encode(clean_html.originalEncoding);
+		list_of_headers.append(heading)
+	for cell in row.findAll('td'):
+		if(logo):
+			data = cell.text.replace('&ndsp;','')
+			data = data.encode(clean_html.originalEncoding);
+			list_of_data.append(data)
+		logo =  1
+
+outfile = open("./company_data.csv","ab")
+writer = csv.writer(outfile)
+writer.writerows(izip(list_of_headers, list_of_data))
+			
